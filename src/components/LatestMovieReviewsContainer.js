3d62c66e7541a36c0c927e80d0689cfb385e359a
replacem3nts <donny.landis@gmail.com>
2020-05-22 00:24:21 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -3,7 +3,32 @@ import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'+ `api-key=${NYT_API_KEY}`;
 
-// Code LatestMovieReviewsContainer Here
+export default class LatestMovieReviewsContainer extends Component {    
+
+    state = {
+        reviews: []
+    }
+
+    handleFetch = () => {
+        fetch(URL)
+            .then(r => r.json())
+            .then(reviews => {
+                this.setState({reviews: reviews.results})})
+    }
+
+    componentDidMount() {
+        this.handleFetch()
+    }
+
+    render() {
+        // console.log(this.state.reviews)
+        return (
+            <div className='latest-movie-reviews'>
+                <MovieReviews reviews={this.state.reviews}/>
+            </div>
+        )
+    }
+}
+
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,29 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
-// Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+
+    
+
+    handleFetch = () => {
+        fetch(URL)
+            .then(r => r.json())
+            .then(reviews => {
+                this.setState({reviews: reviews.results})})
+    }
+
+    render() {
+        return (
+            <div className='searchable-movie-reviews'>
+                <form onSubmit={this.handleFetch}>
+                    <MovieReviews reviews={this.state.reviews}/>
+                </form>
+            </div>
+        )
+    }
+}
